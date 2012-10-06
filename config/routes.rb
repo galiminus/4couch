ab703d67acb61ad6ae599286eccb02f90d9dc225
Victor Goya <bebephoque@gmail.com>
2012-10-06 04:12:17 +0200

    add application layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 FourCouch::Application.routes.draw do
+  resources :communities
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -46,9 +48,7 @@ FourCouch::Application.routes.draw do
   #     resources :products
   #   end
 
-  # You can have the root of your site routed with "root"
-  # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'home#show'
 
   # See how all your routes lay out with "rake routes"
 
