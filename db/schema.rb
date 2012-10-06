889f3b5eaf5dcfd7d0d09ef70351d34bbddab432
Victor Goya <bebephoque@gmail.com>
2012-10-07 00:47:39 +0200

    add username and allow username/email login

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121006201636) do
+ActiveRecord::Schema.define(:version => 20121006221705) do
 
   create_table "communities", :force => true do |t|
     t.string   "name"
@@ -47,6 +47,7 @@ ActiveRecord::Schema.define(:version => 20121006201636) do
     t.datetime "created_at",                             :null => false
     t.datetime "updated_at",                             :null => false
     t.string   "slug"
+    t.string   "username"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
