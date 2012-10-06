02c328eee31895fb90fa4a93d8d20ec2412701fd
Victor Goya <bebephoque@gmail.com>
2012-10-06 21:10:38 +0200

    add user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 FourCouch::Application.routes.draw do
   devise_for :users
+  resources :users
 
   resources :couches
-
   resources :communities
 
   # The priority is based upon order of creation:
