e484c3e40d07075564b97e6ad196f8031799992c
Victor Goya <bebephoque@gmail.com>
2012-10-06 03:18:48 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FourCouch::Application.config.secret_token = '9d73064ee12cf6e15e8edc00d5a69d6e113b32c4b87b48664a2802271c51b81f4de9fe8ac5ca86458d351029068cbadec352c91d52e11fbf60b0faf1e630f1a5'
