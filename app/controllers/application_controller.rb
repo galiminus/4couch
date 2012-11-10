fe6017e3631e494280fcd5e407ba2f3f08f8c8dd
Victor Goya <bebephoque@gmail.com>
2012-11-10 16:11:54 +0100

    remove language selection

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  before_filter :set_locale
+  before_filter :set_locale, :set_search_query_placeholder
 
   def set_locale
     if current_user.present? and current_user.locale.present?
       I18n.locale = current_user.locale
+    else
+      I18n.locale = :en
     end
+  end
+
+  def set_search_query_placeholder
+    @search_placeholder = params[:q] || t(:search_placeholder)
   end
 end
