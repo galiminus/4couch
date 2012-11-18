e664126bb9ecc2e76cb63f9e937f0d852b5bdbc5
Victor Goya <bebephoque@gmail.com>
2012-11-18 01:30:58 +0100

    better style for conversations

--- app/helpers/conversations_helper.rb
+++ app/helpers/conversations_helper.rb
@@ -2,4 +2,20 @@ module ConversationsHelper
   def active_on_recipient(conversation, recipient)
     'active' if conversation.recipient == recipient
   end
+
+  def media_class_for(message, user)
+    if message.user == user
+      "media span7 pagination-right pull-right"
+    else
+      "media span7"
+    end
+  end
+
+  def media_object_link_class_for(message, user)
+    if message.user == user
+      "pull-right"
+    else
+      "pull-left"
+    end
+  end
 end
